9347382c3e2384ed7696bf0d2b6bb21544eff4a8
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-09 18:51:33 +0600

    add api data

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const searchSongs = () => {
     const searchText = document.getElementById('search-field').value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`
+    // load data
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displaySongs(data))
+}
+
+const displaySongs = songs => {
+    console.log(songs);
+    
 }
